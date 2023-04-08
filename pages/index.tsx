a59c5ae32c503bb5e313e48256aa3e6ba87d6692
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-08 11:03:50 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+// Import necessary components and types
 import { Chat } from "@/components/Chat/Chat";
 import { Footer } from "@/components/Layout/Footer";
 import { Navbar } from "@/components/Layout/Navbar";
@@ -5,22 +6,30 @@ import { Message } from "@/types";
 import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 
+// Define the Home component
 export default function Home() {
+  // Create state for messages and loading
   const [messages, setMessages] = useState<Message[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
 
+  // Create a reference for the messages end
   const messagesEndRef = useRef<HTMLDivElement>(null);
 
+  // Define a function to scroll to the bottom of the chat
   const scrollToBottom = () => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
+  // Define a function to handle sending a message
   const handleSend = async (message: Message) => {
+    // Add the new message to the messages list
     const updatedMessages = [...messages, message];
 
+    // Update the messages state and set loading to true
     setMessages(updatedMessages);
     setLoading(true);
 
+    // Make a POST request to the chat API
     const response = await fetch("/api/chat", {
       method: "POST",
       headers: {
@@ -31,24 +40,29 @@ export default function Home() {
       })
     });
 
+    // Check if the response is not ok, then throw an error
     if (!response.ok) {
       setLoading(false);
       throw new Error(response.statusText);
     }
 
+    // Get the response body
     const data = response.body;
 
     if (!data) {
       return;
     }
 
+    // Set loading to false
     setLoading(false);
 
+    // Read the response body using a reader and a decoder
     const reader = data.getReader();
     const decoder = new TextDecoder();
     let done = false;
     let isFirst = true;
 
+    // Read the response body in chunks and update the messages state accordingly
     while (!done) {
       const { value, done: doneReading } = await reader.read();
       done = doneReading;
@@ -76,6 +90,7 @@ export default function Home() {
     }
   };
 
+  // Define a function to reset the chat
   const handleReset = () => {
     setMessages([
       {
@@ -85,10 +100,12 @@ export default function Home() {
     ]);
   };
 
+  // Scroll to the bottom whenever the messages state changes
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
 
+  // Initialize the chat with a welcome message
   useEffect(() => {
     setMessages([
       {
@@ -98,8 +115,10 @@ export default function Home() {
     ]);
   }, []);
 
+  // Render the Home component
   return (
     <>
+      {/* Add meta information for the page */}
       <Head>
         <title>Chatbot UI</title>
         <meta
@@ -110,23 +129,27 @@ export default function Home() {
           name="viewport"
           content="width=device-width, initial-scale=1"
         />
+        {/* Link the favicon for the page */}
         <link
           rel="icon"
           href="/favicon.ico"
         />
       </Head>
 
+      {/* Create a layout with Navbar, Chat, and Footer components */}
       <div className="flex flex-col h-screen">
         <Navbar />
 
         <div className="flex-1 overflow-auto sm:px-10 pb-4 sm:pb-10">
           <div className="max-w-[800px] mx-auto mt-4 sm:mt-12">
+            {/* Render the Chat component with required props */}
             <Chat
               messages={messages}
               loading={loading}
               onSend={handleSend}
               onReset={handleReset}
             />
+            {/* Add a reference for scrolling to the bottom */}
             <div ref={messagesEndRef} />
           </div>
         </div>
@@ -135,3 +158,4 @@ export default function Home() {
     </>
   );
 }
+
