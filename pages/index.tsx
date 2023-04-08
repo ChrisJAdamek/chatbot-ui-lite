73a43c25363f6d3f92c4f6a7e652702f80e19ee8
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-08 11:07:18 +1000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -110,7 +110,7 @@ export default function Home() {
     setMessages([
       {
         role: "assistant",
-        content: `Hi there! I'm Chatbot UI, an AI assistant. I can help you with things like answering questions, providing information, and helping with tasks. How can I help you?`
+        content: `Hi there! I'm Chatbot Legend, an AI assistant. I can help you with things like answering questions, providing information, and helping with tasks. How can I help you?`
       }
     ]);
   }, []);
