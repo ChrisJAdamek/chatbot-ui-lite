d44230428cd27a34e203e0f3dadef29bb4ecf89e
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-08 10:57:16 +1000

    Update index.ts
    Commented

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,14 @@
+// Import necessary types, parser and constants
 import { Message, OpenAIModel } from "@/types";
 import { createParser, ParsedEvent, ReconnectInterval } from "eventsource-parser";
 
+// Define the OpenAIStream function
 export const OpenAIStream = async (messages: Message[]) => {
+  // Create text encoder and decoder instances
   const encoder = new TextEncoder();
   const decoder = new TextDecoder();
 
+  // Fetch chat completion from OpenAI API
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
@@ -26,21 +30,26 @@ export const OpenAIStream = async (messages: Message[]) => {
     })
   });
 
+  // Throw an error if the API response status is not 200
   if (res.status !== 200) {
     throw new Error("OpenAI API returned an error");
   }
 
+  // Create a new ReadableStream
   const stream = new ReadableStream({
     async start(controller) {
+      // Define the onParse function for handling parsed events
       const onParse = (event: ParsedEvent | ReconnectInterval) => {
         if (event.type === "event") {
           const data = event.data;
 
+          // Close the controller if the data is "[DONE]"
           if (data === "[DONE]") {
             controller.close();
             return;
           }
 
+          // Try to parse the data and enqueue it to the controller
           try {
             const json = JSON.parse(data);
             const text = json.choices[0].delta.content;
@@ -52,13 +61,16 @@ export const OpenAIStream = async (messages: Message[]) => {
         }
       };
 
+      // Create the parser and pass the onParse function
       const parser = createParser(onParse);
 
+      // Feed the parser with chunks from the response body
       for await (const chunk of res.body as any) {
         parser.feed(decoder.decode(chunk));
       }
     }
   });
 
+  // Return the ReadableStream instance
   return stream;
 };
